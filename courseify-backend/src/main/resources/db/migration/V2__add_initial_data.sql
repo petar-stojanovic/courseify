a12fc354662b3b182f65245d2fd9b6abfda25c5e
petar-stojanovic <petar-stojanovic@github.com>
2023-07-19 10:24:04 +0200

    added initial data migration

--- courseify-backend/src/main/resources/db/migration/V2__add_initial_data.sql
+++ courseify-backend/src/main/resources/db/migration/V2__add_initial_data.sql
@@ -1,6 +1,18 @@
-insert into comics (name)
-values ('DC');
+insert into course_category (category_name)
+values ('Java Programming'),
+       ('Python Programming');
 
-insert into heroes (name, power, is_super_hero, comic_id)
-values ('Superman', 100, true, 1),
-       ('Batman', 100, true, 1);
+insert into role (role_name)
+values ('Admin'),
+       ('User');
+
+
+insert into users (first_name, last_name, email, password, username, role_id)
+values ('Petar', 'Stojanovic', 'petar@gmail.com', 'Test123!', 'petar', 1),
+       ('Bojan', 'Ristevski', 'bojan@gmail.com', 'Test123!', 'bojan', 2);
+
+
+insert into course (title, description, author_id, category_id)
+values ('Java Programming Course', 'Java Programming Course Description', 1, 1),
+       ('Python Programming Course For Beginners', 'Python Programming Course For Beginners Description', 2, 2);
+
