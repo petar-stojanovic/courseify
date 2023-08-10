10ccdcbe448a5c3e2bde1355f65e2a109414284d
petar-stojanovic <petar-stojanovic@github.com>
2023-08-10 15:21:24 +0200

    removing comments from import

--- courseify-backend/build.gradle.kts
+++ courseify-backend/build.gradle.kts
@@ -29,18 +29,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.1.1")
 
-
-    /**
-     * Oauth2
-     * */
-    implementation("org.springframework.boot:spring-boot-starter-security")
-//    implementation("org.springframework.security:spring-security-oauth2-jose")
-//    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-
-
     /**
      * Security
      */
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
